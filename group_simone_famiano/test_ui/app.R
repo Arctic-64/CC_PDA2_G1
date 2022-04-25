73f51267a2eb97dc368d6cc78a872a3cadf631c3
Slonna1 <slonna23@gmail.com>
2022-04-26 00:16:57 +0100

    App ui + new dataset
    Some cleaning on app ui
    Created data set for 2021Q4

--- group_simone_famiano/test_ui/app.R
+++ group_simone_famiano/test_ui/app.R
@@ -15,20 +15,28 @@ ui <- fluidPage(
 titlePanel("Demographic data"),
 
  fluidRow(
+   #column(3,
+    #      radioButtons("tot_avg_input",
+     #                  "Select Total Stays or Average Stay Length",
+      #                 choices = list("Total Stays" = tot_and_avg_stays$tot_stays,
+       #                               "Average Length of Stay" = tot_stays_age$avg_stays),
+        #               selected = 1),
+         # ),
+   
    column(3,
    pickerInput("age_input", 
                "Select Age Groups", 
-               choices = unique(tot_stays_age$age),
+               choices = unique(tot_stays_age_no_location$age),
                multiple =  TRUE,
-               selected = unique(tot_stays_age$age)),
+               selected = unique(tot_stays_age_no_location$age)),
    ),
    
    column(6,
    pickerInput("quarter_input", 
                "Select Quarter", 
-               choices = unique(tot_stays_age$quarter),
+               choices = unique(tot_stays_age_no_location$quarter),
                multiple =  TRUE,
-               selected = unique(tot_stays_age$quarter))
+               selected = unique(tot_stays_age_no_location$quarter))
         )
    ),
  
@@ -48,30 +56,31 @@ server <- function(input, output) {
 
 output$table_output <- renderDataTable({
   
-  tot_stays_age %>% 
-    filter(age %in% input$age_input)%>% 
-    filter(quarter %in% input$quarter_input) 
+  dashboard_data_table %>% 
+    filter(Age %in% input$age_input)%>% 
+    filter(Quarter %in% input$quarter_input) 
 })
 
 
   output$age_plot <- renderPlot({
-    tot_stays_age %>% 
+    tot_stays_age_no_location %>%
       filter(age %in% input$age_input) %>% 
-      filter(quarter %in% input$quarter_input) %>% 
+      filter(quarter %in% input$quarter_input) %>%
         ggplot() +
-          geom_col(position = "dodge", aes(x = age, 
+          geom_col(position = "dodge", colour = "white", aes(x = age, 
                                            y = tot_stays, 
                                            fill = quarter)
                     ) +
           ggtitle("Total Stays per Age") +
           labs(x = "Age", y = "Total Stays") +
-          scale_fill_manual(values = c("2020Q1" = "#004785",
-                                       "2020Q2" = "#99DAF5",
-                                       "2020Q3" = "#004796",
-                                       "2020Q4" = "#99DAF9",
-                                       "2021Q1" = "#004707",
-                                       "2021Q2" = "#99DAF0",
-                                       "2021Q3" = "#004718")) +
+          scale_fill_manual(values = c("2020Q1" = "#061a1f",
+                                       "2020Q2" = "#062e3c",
+                                       "2020Q3" = "#074859",
+                                       "2020Q4" = "#11667f",
+                                       "2021Q1" = "#008b87",
+                                       "2021Q2" = "#47899b",
+                                       "2021Q3" = "#659799")
+                            ) +
           facet_wrap(~sex) +
           theme_bw()
     
