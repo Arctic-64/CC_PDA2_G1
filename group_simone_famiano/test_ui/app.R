7c870fab31996e497c355e1b370994da5ba961ad
Slonna1 <slonna23@gmail.com>
2022-04-26 16:10:24 +0100

    UI
    possible final Demographics user interface

--- group_simone_famiano/test_ui/app.R
+++ group_simone_famiano/test_ui/app.R
@@ -1,52 +1,67 @@
 
 library(shiny)
 library(shinyWidgets)
+library(DT)
 library(tidyverse)
 
 
-# run data from notebook
-
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
+  
 
   # Application title
-titlePanel("Demographic data"),
 
- fluidRow(
-   #column(3,
-    #      radioButtons("tot_avg_input",
-     #                  "Select Total Stays or Average Stay Length",
-      #                 choices = list("Total Stays" = tot_and_avg_stays$tot_stays,
-       #                               "Average Length of Stay" = tot_stays_age$avg_stays),
-        #               selected = 1),
-         # ),
-   
+h1(strong("Demographics Data"), align="center", style = "font-size:100px;"),
+
+tags$head(tags$style('
+   body {
+      font-family: Arial; 
+      font-size: 20px; 
+   }'
+)),
+
+
+br(),
+br(),
+
+  fluidRow(  
    column(3,
-   pickerInput("age_input", 
+          
+          
+          
+          pickerInput("age_input", 
                "Select Age Groups", 
-               choices = unique(tot_stays_age_no_location$age),
+               choices = unique(tot_and_avg_stays$age),
                multiple =  TRUE,
-               selected = unique(tot_stays_age_no_location$age)),
-   ),
+               selected = unique(tot_and_avg_stays$age)),
+          ),
    
-   column(6,
+      
+   
+   column(9,
    pickerInput("quarter_input", 
                "Select Quarter", 
-               choices = unique(tot_stays_age_no_location$quarter),
+               choices = unique(tot_and_avg_stays$quarter),
                multiple =  TRUE,
-               selected = unique(tot_stays_age_no_location$quarter))
-        )
-   ),
+               selected = unique(tot_and_avg_stays$quarter))
+        
+   )
+ ),
  
  
-          plotOutput("age_plot"),
+          plotOutput("tot_stays_plot"),
+
+br(),
+br(),
+
+
+          plotOutput("avg_length_plot"),
+          
  
  
          dataTableOutput("table_output")
     
- )   
+)   
 
  
 
@@ -58,12 +73,22 @@ output$table_output <- renderDataTable({
   
   dashboard_data_table %>% 
     filter(Age %in% input$age_input)%>% 
-    filter(Quarter %in% input$quarter_input) 
+    filter(Quarter %in% input$quarter_input) %>% 
+    arrange(Location)
+  
+  datatable(dashboard_data_table, escape = FALSE, selection = "none") %>% 
+    formatStyle("Gender",
+                  target = "row",
+                  backgroundColor = styleEqual(c("Female", "Male"),
+                                               c("#062e3c", "#074859"))
+    ) %>% 
+    formatStyle(columns = names(dashboard_data_table), color = "white",
+                fontWeight = "bold", family = "Arial")
 })
 
 
-  output$age_plot <- renderPlot({
-    tot_stays_age_no_location %>%
+  output$tot_stays_plot <- renderPlot({
+    tot_and_avg_stays %>%
       filter(age %in% input$age_input) %>% 
       filter(quarter %in% input$quarter_input) %>%
         ggplot() +
@@ -71,9 +96,9 @@ output$table_output <- renderDataTable({
                                            y = tot_stays, 
                                            fill = quarter)
                     ) +
-          ggtitle("Total Stays per Age") +
+          ggtitle("Total Stays") +
           labs(x = "Age", y = "Total Stays") +
-          scale_fill_manual(values = c("2020Q1" = "#061a1f",
+          scale_fill_manual("Quarter", values = c("2020Q1" = "#061a1f",
                                        "2020Q2" = "#062e3c",
                                        "2020Q3" = "#074859",
                                        "2020Q4" = "#11667f",
@@ -82,7 +107,40 @@ output$table_output <- renderDataTable({
                                        "2021Q3" = "#659799")
                             ) +
           facet_wrap(~sex) +
-          theme_bw()
+          theme_bw() +
+          theme(text=element_text(size = 20,  family = "Avalon Regular"),
+                strip.background =element_rect(fill = "#062e3c"),
+                strip.text = element_text(colour = 'white', size = 18,
+                                          family = "Arial"))
+    
+  })
+  
+  output$avg_length_plot <- renderPlot({
+    tot_and_avg_stays %>% 
+      filter(age %in% input$age_input) %>% 
+      filter(quarter %in% input$quarter_input) %>%
+      select(age, average_length_of_stay, quarter, sex) %>% 
+      ggplot() +
+      geom_col(position = "dodge",  
+               colour = "white", aes(x = age, y = average_length_of_stay, 
+                                                         fill = quarter)
+      ) +
+      ggtitle("Average Length of Stay") +
+      labs(x = "Age", y = "Average Length of Stay") +
+      scale_fill_manual("Quarter", values = c("2020Q1" = "#061a1f",
+                                   "2020Q2" = "#062e3c",
+                                   "2020Q3" = "#074859",
+                                   "2020Q4" = "#11667f",
+                                   "2021Q1" = "#008b87",
+                                   "2021Q2" = "#47899b",
+                                   "2021Q3" = "#659799")
+                        ) +
+      facet_wrap(~sex) +
+      theme_bw() +
+      theme(text=element_text(size = 20,  family = "Arial"),
+            strip.background =element_rect(fill = "#074859"),
+            strip.text = element_text(colour = 'white', size = 18,
+                                      family = "Arial"))
     
   })
 }
