53158f481f91ff29ab52c213c3e3a7e79e3fd576
Grant Walker <grantwalkergw@gmail.com>
2022-04-27 10:14:15 +0100

    adds: shiny app updates for layout

--- shiny_dashboard_gw/app.R
+++ shiny_dashboard_gw/app.R
@@ -1,36 +1,54 @@
 library(shiny)
 library(tidyverse)
 
-data <- read_csv("clean_data/activity_by_board_of_treatment_and_speciality.csv")
+data <- read_csv("../shiny_dashboard_gw/data/phs_admissions_data_clean.csv")
 hb_name <- unique(data$hb_name)
 hb_urban <- unique(data$urban_rural)
 
 ui <- fluidPage(
   
-  titlePanel("group_project_geography"),
+  titlePanel(h1("National variation in hospital activity", align = "center")),
   
   fluidRow(
+    
+    titlePanel(h1("Health board", align = "center")),
+    
+    column(6,
+           selectInput(inputId = "hb_input", 
+                       label = "Select NHS Health Board", 
+                       choices = hb_name)),
+    
     column(6,
       selectInput(inputId = "hb_input", 
                   label = "Select NHS Health Board", 
                   choices = hb_name)),
+    ),
+  
+  fluidRow(
     
-    column(6,
-      radioButtons(inputId = "urban_input", 
-                   label = "Urban or rural?",
-                   choices = hb_urban)),
-      
+    # column(6,
+    #   radioButtons(inputId = "urban_input", 
+    #                label = "Urban or rural?",
+    #                choices = hb_urban)),
+    
+    column(5,
+           plotOutput("hb_quarter")),
+    
+    column(7,
+           plotOutput("hb_activity")),
+    
     ),
     
-    fluidRow(
-      column(4,
-      plotOutput("hb_activity")),
-      
-      column(4,
-      plotOutput("hb_quarter")),
-      
-      column(4,
+  fluidRow(
+    
+    titlePanel(h1("Urban or rural", align = "center")),
+    
+    column(6,
       plotOutput("hb_urban")),
+    
+    column(6,
+           plotOutput("hb_urban_rural_time")),
+    
     )
   )
 
@@ -58,12 +76,12 @@ server <- function(input, output) {
     geom_point(aes(x = quarter, y = episodes, colour = hb_name), alpha = 0.75) +
     xlab("Quarter") +
     ylab("Episodes") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))})
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")})
   
   output$hb_urban <- renderPlot({
     data %>%
       filter(hb_code != "S27000001", hb_code != "SN0811", hb_name != "Scotland") %>% 
-      filter(urban_rural == input$urban_input) %>% 
+      #filter(urban_rural == input$urban_input) %>% 
       group_by(urban_rural, admission_type) %>% 
       summarise(episodes = sum(episodes/total_occupied_beds)) %>% 
       ggplot() +
@@ -73,6 +91,23 @@ server <- function(input, output) {
       xlab("Admission type") +
       ylab("Episodes") +
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))})
+  
+  output$hb_urban_rural_time <- renderPlot({
+    
+    data %>%
+      filter(hb_name != "National Facility NHS Louisa Jordan", hb_name != "Non-NHS Provider Location", hb_name != "The Golden Jubilee National Hospital") %>% # remove hb with pop NA
+      filter(hb_code != "S27000001", hb_name != "National Facility NHS Louisa Jordan") %>% 
+      group_by(quarter, urban_rural) %>% 
+      summarise(episodes = sum(episodes)/mean(pop)) %>% 
+      ggplot() +
+      aes(x = quarter, y = episodes, group = urban_rural, colour = urban_rural) +
+      geom_line() +
+      ylab("Episodes per capita") +
+      ylim(0,NA)
+    
+  })
+  
+  
   
 }
 
