c3ffcdde6d97cd3abf1e25f59e5f5a0aef8a77c2
Grant Walker <grantwalkergw@gmail.com>
2022-04-27 12:47:02 +0100

    adds: lunch update

--- shiny_dashboard_gw/app.R
+++ shiny_dashboard_gw/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(shinyWidgets)
 
 data <- read_csv("../shiny_dashboard_gw/data/phs_admissions_data_clean.csv")
 hb_name <- unique(data$hb_name)
@@ -11,17 +12,22 @@ ui <- fluidPage(
   
   fluidRow(
     
-    titlePanel(h1("Health board", align = "center")),
-    
+    titlePanel(h2("Health board")),
+    br(),
+    p("Explore the differences between (left) and within (right) the Health boards of Scotland. Within a Health board the variation in admissions is shown."),
+    br(),
     column(6,
-           selectInput(inputId = "hb_input", 
-                       label = "Select NHS Health Board", 
-                       choices = hb_name)),
-    
+            pickerInput(inputId = "multi_hb_input", 
+                       label = "Select Health boards", 
+                       choices = unique(data$hb_name),
+                       multiple = TRUE,
+                       selected = unique(data$hb_name))),
+           
     column(6,
-      selectInput(inputId = "hb_input", 
-                  label = "Select NHS Health Board", 
-                  choices = hb_name)),
+            selectInput(inputId = "hb_input", 
+                              label = "Select NHS Health Board", 
+                              choices = hb_name)),
+           
     ),
   
   fluidRow(
@@ -31,77 +37,85 @@ ui <- fluidPage(
     #                label = "Urban or rural?",
     #                choices = hb_urban)),
     
-    column(5,
+    column(6,
            plotOutput("hb_quarter")),
     
-    column(7,
+    column(6,
            plotOutput("hb_activity")),
     
-    ),
-    
+  ),
+  
+  br(),
+  
   fluidRow(
     
-    titlePanel(h1("Urban or rural", align = "center")),
-    
+    titlePanel(h2("Urban or rural")),
+    br(),
+    p("The differences between urban and rural Health board admissions are presented on a per capita basis."),
+    br(),
     column(6,
-      plotOutput("hb_urban")),
+           plotOutput("hb_urban")),
     
     column(6,
            plotOutput("hb_urban_rural_time")),
     
-    )
   )
+)
 
 server <- function(input, output) {
   
   output$hb_activity <- renderPlot({
     data %>% 
-    filter(hb_name == input$hb_input) %>%
-    group_by(quarter, admission_type) %>%  
-    summarise(episodes = sum(episodes/1000)) %>% 
-    ggplot() +
-    geom_line(aes(x = quarter, y = episodes, group = admission_type, colour = admission_type), alpha = 1) +
-    geom_point(aes(x = quarter, y = episodes, colour = admission_type), alpha = 1) +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-    xlab("Admission type") +
-    ylab("Thousands of episodes")})
+      filter(hb_name == input$hb_input) %>%
+      group_by(quarter, admission_type) %>%  
+      summarise(episodes = sum(episodes)) %>% 
+      ggplot() +
+      geom_line(aes(x = quarter, y = episodes, group = admission_type, colour = admission_type), alpha = 1) +
+      geom_point(aes(x = quarter, y = episodes, colour = admission_type), alpha = 0.5, size = 0.75) +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+      xlab("Admission type") +
+      ylab("Episodes")
+      })
   
   output$hb_quarter <- renderPlot({
     data %>%
-    filter(hb_name == input$hb_input) %>%
-    group_by(quarter, hb_name) %>%
-    summarise(episodes = sum(episodes/total_occupied_beds)) %>%
-    ggplot() +
-    geom_line(aes(x = quarter, y = episodes, group = hb_name, colour = hb_name), alpha = 0.75) +
-    geom_point(aes(x = quarter, y = episodes, colour = hb_name), alpha = 0.75) +
-    xlab("Quarter") +
-    ylab("Episodes") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")})
+      filter(hb_name == input$multi_hb_input) %>%
+      filter(hb_name != "Non-NHS Provider Location", hb_name != "The Golden Jubilee National Hospital") %>% # remove hb with pop NA
+      group_by(quarter, hb_name) %>% 
+      summarise(episodes = sum(episodes)/mean(pop)) %>% 
+      ggplot() +
+      geom_line(aes(x = quarter, y = episodes, group = hb_name, colour = hb_name), alpha = 0.75) +
+      geom_point(aes(x = quarter, y = episodes, colour = hb_name), alpha = 0.75) +
+      xlab("Quarter") +
+      ylab("Episodes per capita") +
+      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      })
   
   output$hb_urban <- renderPlot({
     data %>%
-      filter(hb_code != "S27000001", hb_code != "SN0811", hb_name != "Scotland") %>% 
-      #filter(urban_rural == input$urban_input) %>% 
+      filter(hb_name != "Non-NHS Provider Location", hb_name != "The Golden Jubilee National Hospital") %>% # remove hb with pop NA
+      filter(hb_code != "S27000001") %>% # urban_rural status unclear
       group_by(urban_rural, admission_type) %>% 
-      summarise(episodes = sum(episodes/total_occupied_beds)) %>% 
+      summarise(episodes = sum(episodes)/mean(pop)) %>% 
       ggplot() +
       aes(x = admission_type, y = episodes) +
       geom_bar(stat = "identity") +
       facet_grid(~urban_rural) +
       xlab("Admission type") +
-      ylab("Episodes") +
+      ylab("Episodes per capita") +
       theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))})
   
   output$hb_urban_rural_time <- renderPlot({
     
     data %>%
-      filter(hb_name != "National Facility NHS Louisa Jordan", hb_name != "Non-NHS Provider Location", hb_name != "The Golden Jubilee National Hospital") %>% # remove hb with pop NA
-      filter(hb_code != "S27000001", hb_name != "National Facility NHS Louisa Jordan") %>% 
+      filter(hb_name != "Non-NHS Provider Location", hb_name != "The Golden Jubilee National Hospital") %>% # remove hb with pop NA
+      filter(hb_code != "S27000001") %>% 
       group_by(quarter, urban_rural) %>% 
       summarise(episodes = sum(episodes)/mean(pop)) %>% 
       ggplot() +
       aes(x = quarter, y = episodes, group = urban_rural, colour = urban_rural) +
       geom_line() +
+      xlab("Quarter") +
       ylab("Episodes per capita") +
       ylim(0,NA)
     
